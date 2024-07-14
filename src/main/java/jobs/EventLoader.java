0d6257819eb55fadad5259be115531015581659d
cykei <kelle111@naver.com>
2024-07-14 20:37:54 +0900

    추가파일을 inputDir 에 넣고 실행하면 처리할 수 있게 변경

--- src/main/java/jobs/EventLoader.java
+++ src/main/java/jobs/EventLoader.java
@@ -1,22 +1,30 @@
 package jobs;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 import static org.apache.spark.sql.functions.*;
 
 public class EventLoader {
     public static void main(String[] args) throws Exception {
+
         // 파일 경로 및 기타 변수 설정
-        String inputPath = "hdfs://namenode:9000/user/hive/warehouse/data/*.csv";
+        String inputDir = "hdfs://namenode:9000/user/hive/warehouse/data";
         String outputPath = "hdfs://namenode:9000/user/hive/warehouse/output";
+        String checkPointPath = "hdfs://namenode:9000/user/hive/warehouse/output/checkpoint";
         String tableName = "event";
         String zoneId = "Asia/Seoul";
 
@@ -25,6 +33,17 @@ public class EventLoader {
                 .appName("eventLoader")
                 .enableHiveSupport()
                 .getOrCreate();
+
+        spark.sparkContext().setCheckpointDir(checkPointPath);
+
+        Configuration hadoopConf = new Configuration();
+        FileSystem fileSystem = FileSystem.get(hadoopConf);
+        FileStatus[] inputFileStatuses = fileSystem.listStatus(new Path(inputDir));
+        FileStatus[] checkFileStatuses = fileSystem.listStatus(new Path(checkPointPath));
+        List<String> inputFileNames = getFileNames(inputFileStatuses);
+        List<String> checkFileNames = getFileNames(checkFileStatuses);
+
+        List<String> targetFilenames = getDifference(inputFileNames, checkFileNames);
 
         List<StructField> fields = new ArrayList<>();
         fields.add(DataTypes.createStructField("event_time", DataTypes.StringType, false));
@@ -40,24 +59,31 @@ public class EventLoader {
         StructType schema = DataTypes.createStructType(fields);
 
         // 데이터 로드 및 처리
-        Dataset<Row> df = spark.read()
-                .schema(schema)
-                .option("header", "true")
-                .option("sep", ",")
-                .csv(inputPath);
+        for (String filename : targetFilenames) {
+            Path checkFilePath = new Path(checkPointPath, filename + ".check");
+            if (!fileSystem.createNewFile(checkFilePath)) {
+                throw new IOException("checkDir 이 존재하는지 확인해주세요.");
+            }
 
-        Dataset<Row> formattedDF = df
-                .withColumn("event_time_utc",
-                        date_format(to_timestamp(regexp_replace(col("event_time"), " UTC$", "")), "yyyy-MM-dd HH:mm:ss"))
-                .withColumn("event_time_kst", from_utc_timestamp(col("event_time_utc"), zoneId))
-                .withColumn("partition_key", date_format(col("event_time_kst"), "yyyy-MM-dd"));
+            String inputFilePath = String.join("/", inputDir, filename);
+            Dataset<Row> df = spark.read()
+                    .schema(schema)
+                    .option("header", "true")
+                    .option("sep", ",")
+                    .csv(inputFilePath);
 
-        // 데이터를 Parquet 포맷으로 저장
-        formattedDF.write()
-                .mode("overwrite")
-                .partitionBy("partition_key")
-                .format("parquet")
-                .save(outputPath + "/parquet");
+            Dataset<Row> formattedDF = df
+                    .withColumn("event_time_utc",
+                            date_format(to_timestamp(regexp_replace(col("event_time"), " UTC$", "")), "yyyy-MM-dd HH:mm:ss"))
+                    .withColumn("event_time_kst", from_utc_timestamp(col("event_time_utc"), zoneId))
+                    .withColumn("partition_key", date_format(col("event_time_kst"), "yyyy-MM-dd"));
+
+            // 데이터를 Parquet 포맷으로 저장
+            formattedDF.write()
+                    .mode(SaveMode.Append)
+                    .partitionBy("partition_key")
+                    .parquet(outputPath+"/parquet");
+        }
 
         // Hive External Table 생성
         spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS " + tableName + " ("
@@ -79,5 +105,22 @@ public class EventLoader {
         // 테이블 데이터 복구
         spark.sql("MSCK REPAIR TABLE " + tableName);
         spark.stop();
+    }
+
+    private static List<String> getFileNames(FileStatus[] inputFileStatuses) {
+        List<String> fileNames = new ArrayList<>();
+        for (FileStatus fileStatus : inputFileStatuses) {
+            fileNames.add(fileStatus.getPath().getName());
+        }
+        return fileNames;
+    }
+
+    private static List<String> getDifference(List<String> sources, List<String> targets) {
+        Set<String> all = new HashSet<>(sources);
+        for (String filename : targets) {
+            String filenameWithoutExtension = FilenameUtils.removeExtension(filename);
+            all.remove(filenameWithoutExtension);
+        }
+        return new ArrayList<>(all);
     }
 }
--- src/main/java/jobs/EventLoaderLocal.java
+++ src/main/java/jobs/EventLoaderLocal.java
@@ -1,5 +1,6 @@
 package jobs;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -7,8 +8,9 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -16,8 +18,9 @@ public class EventLoaderLocal {
     public static void main(String[] args) throws Exception {
         // 파일 경로 및 기타 변수 설정
         System.setProperty("hadoop.home.dir", "C:\\Users\\cykei\\hadoop2.7.3");
-        String inputPath = "D:/dockerProjects/hadoop_base/hive/data/*.csv";
+        String inputDir = "D:/dockerProjects/hadoop_base/hive/data";
         String outputPath = "D:/dockerProjects/hadoop_base/hive/data/output";
+        String checkPointPath = "D:/dockerProjects/hadoop_base/hive/data/output/checkpoint";
         String tableName = "event";
         String zoneId = "Asia/Seoul";
 
@@ -25,9 +28,11 @@ public class EventLoaderLocal {
                 .builder()
                 .master("local[*]")
                 .appName("eventLoader")
+                .config("checkPointLocation", checkPointPath)
                 .enableHiveSupport()
                 .getOrCreate();
 
+        spark.sparkContext().setCheckpointDir(checkPointPath);
 
         List<StructField> fields = new ArrayList<>();
         fields.add(DataTypes.createStructField("event_time", DataTypes.StringType, false));
@@ -44,47 +49,74 @@ public class EventLoaderLocal {
 
 
         // 데이터 로드 및 처리
-        Dataset<Row> df = spark.read()
-                .schema(schema)
-                .option("header", "true")
-                .option("sep", ",")
-                .csv(inputPath);
+        File file = new File(inputDir);
+        String[] fileNames = file.list((f, name) -> name.endsWith("csv"));
 
-        Dataset<Row> formattedDF = df
-                .withColumn("event_time_utc",
-                        date_format(to_timestamp(regexp_replace(col("event_time"), " UTC$", "")), "yyyy-MM-dd HH:mm:ss"))
-                .withColumn("event_time_kst", from_utc_timestamp(col("event_time_utc"), zoneId))
-                .withColumn("partition_key", date_format(col("event_time_kst"), "yyyy-MM-dd"));
+        File check = new File(checkPointPath);
+        String[] checkedNames = check.list((f, name) -> name.endsWith("check"));
 
-        formattedDF.show();
-        formattedDF.printSchema();
+        if (fileNames == null) {
+            throw new Exception("입력 데이터가 없습니다.");
+        }
 
-        // 데이터를 Parquet 포맷으로 저장
-        formattedDF.write()
-                .mode("overwrite")
-                .partitionBy("partition_key")
-                .format("parquet")
-                .save(outputPath + "/parquet");
+        List<String> targetFilenames = getDifference(fileNames, checkedNames);
 
-        // Hive External Table 생성
-        spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS " + tableName + " ("
-                + "event_time STRING, "
-                + "event_type STRING, "
-                + "product_id INT, "
-                + "category_id STRING, "
-                + "category_code STRING, "
-                + "brand STRING, "
-                + "price DOUBLE, "
-                + "user_id INT, "
-                + "user_session STRING, "
-                + "event_time_kst TIMESTAMP "
-                + ") "
-                + "PARTITIONED BY (partition_key STRING) "
-                + "STORED AS PARQUET "
-                + "LOCATION '" + outputPath + "/parquet'");
+        for (String filename : targetFilenames) {
+            File checked = new File(checkPointPath, filename + ".check");
+            if (!checked.createNewFile()) {
+                throw new IOException("checkPointPath 가 존재하는지 확인해주세요.");
+            }
+            String inputPath = new File(inputDir, filename).getPath();
+            Dataset<Row> df = spark.read()
+                    .schema(schema)
+                    .option("header", "true")
+                    .option("sep", ",")
+                    .csv(inputPath);
 
-        // 테이블 데이터 복구
-        spark.sql("MSCK REPAIR TABLE " + tableName);
+            Dataset<Row> formattedDF = df
+                    .withColumn("event_time_utc",
+                            date_format(to_timestamp(regexp_replace(col("event_time"), " UTC$", "")), "yyyy-MM-dd HH:mm:ss"))
+                    .withColumn("event_time_kst", from_utc_timestamp(col("event_time_utc"), zoneId))
+                    .withColumn("partition_key", date_format(col("event_time_kst"), "yyyy-MM-dd"));
+
+            // 데이터를 Parquet 포맷으로 저장
+            formattedDF.write()
+                    .mode("append")
+                    .partitionBy("partition_key")
+                    .format("parquet")
+                    .save(outputPath + "/parquet");
+
+        }
+
+//        // Hive External Table 생성
+//        spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS " + tableName + " ("
+//                + "event_time STRING, "
+//                + "event_type STRING, "
+//                + "product_id INT, "
+//                + "category_id STRING, "
+//                + "category_code STRING, "
+//                + "brand STRING, "
+//                + "price DOUBLE, "
+//                + "user_id INT, "
+//                + "user_session STRING, "
+//                + "event_time_kst TIMESTAMP "
+//                + ") "
+//                + "PARTITIONED BY (partition_key STRING) "
+//                + "STORED AS PARQUET "
+//                + "LOCATION '" + outputPath + "/parquet'");
+//
+//        // 테이블 데이터 복구
+//        spark.sql("MSCK REPAIR TABLE " + tableName);
+
         spark.stop();
+    }
+
+    private static List<String> getDifference(String[] sources, String[] targets) {
+        Set<String> all = new HashSet<>(Arrays.asList(sources));
+        for (String filename : targets) {
+            String filenameWithoutExtension = FilenameUtils.removeExtension(filename);
+            all.remove(filenameWithoutExtension);
+        }
+        return new ArrayList<>(all);
     }
 }
